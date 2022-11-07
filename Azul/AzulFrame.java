71a43579099803f68cf45a6cf6d0f22544c314fc
Tinnylattice <vladimirs.sterbs@inbox.lv>
2022-11-07 08:01:04 -0600

    Update frame class fo show

--- Azul/AzulFrame.java
+++ Azul/AzulFrame.java
@@ -1,9 +1,14 @@
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class AzulFrame extends JFrame {
     private static int HEIGHT = 900;
     private static int WIDTH = 1600;
+
     public AzulFrame() {
-        
+    setSize(WIDTH,HEIGHT);
+    setTitle("Azul");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    setVisible(true);  
     }
+//System.out.println(e.getX() + " / " + e.getY());
 }
