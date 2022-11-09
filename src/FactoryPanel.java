4212297856e3ab554d2c700d3a5d50cb63a1ed02
rkasibhatla6 <sairavi.kasibhatla@live.com>
2022-11-09 07:20:15 -0600

    pls work

--- src/FactoryPanel.java
+++ src/FactoryPanel.java
@@ -1,3 +1,30 @@
+import java.awt.*;
+import java.awt.image.*;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.io.*;
+import java.util.ArrayList;
 public class FactoryPanel {
-    
+    public BufferedImage factory, black, blue, red, white, yellow;
+    public Factory f1, f2, f3, f4, f5, f6, f7, f8, f9;
+    public ArrayList<Token> center;
+    public FactoryPanel(){
+        /*factory = ImageIO.read(FactoryPanel.class.getResource("/Image/factory.png"));
+        black = ImageIO.read("black.png");
+        */
+        //factory algo: f1 = new Factory();
+
+    }
+
+    public void drawTokens(Graphics g){
+        
+    }
+
+    public void drawFactories(Graphics g){
+
+    }
+
+    public void drawCenter(Graphics g){
+
+    }
 }
